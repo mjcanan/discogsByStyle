bd8e321b5217e6d742ef43065fcef194f2a8f45d
mjcanan <matthew.canan@gmail.com>
2020-09-13 16:20:03 -0400

    added print all feature and documentation

--- README.md
+++ README.md
@@ -1,33 +1,54 @@
-# Discogs Sorting App
-Retrieves and sorts your discogs collection so you can view albums by style and genre
+# Discogs Sorting Program
+Retrieves and sorts your Discogs collection so you can view albums by style and genre
 # Description
 Discogs.com is an online database where users can keep track of their record collection.
 As of 9/1/2020, Discogs's website and its API do not allow for users to sort their collections by style.
 This program will retrieve your collection from your "Uncategorized" folder and allow you to sort your collection
 by style and genre.  Once a sort method is chosen, the program will return a numbered list of all the records in your
-collection which belong to that genre.
+collection which belong to that style or genre.  Included is an option to print all of your records, with information 
+about style and genre included.
 # Usage
-To use this program, you will need a your discog's username and an authorization token.
+To use this program, you will need your Discogs username and an authorization token.
 An authorization token can be found <a href="https://www.discogs.com/settings/developers">here</a>.
 You may need to sign in first.
 <br>
 <br>
 Once you have your token, enter the following on the command line:
 <br>
-discogsByStyle.py [user name] [token]
-<br>
-
-<u>Flags</u>:<br>
--h for help
-
-<u>Commands</u>:<br>
-k: get sort keys<br>
-g: sort by genre<br>
-s: sort by style<br>
--h: help<br>
-q: quit<br>
-
+<pre>python discogsByStyle.py [user name] [token]</pre>
+###Flags
+-h: Help.  Prints usage
+###Commands
+ k: Prints style and genre keys<br>
+ a: Will print all records in your collection, sorted by artist name<br>
+ s: Returns all records in your collection that match the selected style, sorted by artist name<br>
+ g: Returns all records in your collection that match the selected genre, sorted by artist name<br>
+-h: Help<br>
+ q: Quit
 # Return Format
-[#]. [Artist Name] - [Record Title] --- [List of All Genres or Styles for the Record]<br>.<br>.
-<br>-------------------------------------------------------<br>
-[Total Records Returned] [Percentage of Total Collection]
+###Sorting by Style or Genre
+<pre>[#]. [Artist Name] - [Record Title] --- [List of All Genres or Styles for the Record]
+.
+.
+-------------------------------------------------------
+Total: [Total Records Returned]. Percentage of Collection = [Percentage of Total Collection] %
+</pre>
+###Printing All
+<pre>[#]. [Artist Name] - [Record Title]
+    Styles: [Styles]
+    Genres: [Genres]
+.
+.
+-------------------------------------------------------
+Total: [Total Records Returned]</pre>
+#Limitations
+Printing all records while sorting by style and genre is currently not supported, although I'm not sure that would be a 
+useful feature.  Sorting categorized folders is also currently not supported, but may be implemented in the future, 
+along with additional sort criteria, such as a "Sort by Decade" feature.<br><br>
+Styles and genres must be typed exactly as they appear (including punctuation) or else no results will be returned.  
+This makes searching for longer styles and genres (.e.g "Folk, World, & Country") a little tedious.<br><br>
+A token must be obtained before using this program, and only your personal collection can be viewed. Discogs supports
+OAuth, but requires additional steps to register and use the service.  Since the purpose of this app is to sort and view
+your own collection and not the collection of others, it was easier to implement with a token for personal use than to
+go through the trouble of registering and rewriting the program.  Nothing is written in stone, however, so this may 
+change in a future version.
--- discogsByStyle.py
+++ discogsByStyle.py
@@ -8,7 +8,7 @@ class Record:
         self.title = t
         self.genres = g
         self.styles = s
-
+        #self.year = y
 
 
 def main():
@@ -16,16 +16,21 @@ def main():
     style_list = []
     coll_size = 0
 
+    # Error Check for proper command line inputs
     if len(sys.argv) != 3 or "-h" in sys.argv:
         print("Usage: discogsByStyle.py <username> <token>\nFind a token here: discogs.com/settings/developers")
         sys.exit()
 
-    print("\t***********DISCOGS BY STYLE***********\n" +
-    "\t* This app will sort your discogs    *\n" +
-    "\t* collection by style or genre.      *\n" +
-    "\t* Enter '-h' at anytime for help.    *\n" +
-    "\t**************************************\n\nLoading your Collection now.  This may take a few seconds...")
+    print('''
+            ***********DISCOGS BY STYLE***********
+            * This app will sort your discogs    *
+            * collection by style or genre.      *
+            * Enter '-h' at anytime for help.    *
+            **************************************
 
+Loading your Discogs collection now.  This may take a few seconds...''')
+
+    # Obtain collection from Discogs, sort by artist name, then sort genres and styles alphabetically
     collection = get_vinyl(sys.argv)
     f_collection = format_out(collection, genre_list, style_list)
     f_collection.sort(key=lambda x: x.artist)
@@ -33,17 +38,25 @@ def main():
     genre_list.sort()
     style_list.sort()
 
+    #TODO: add feature to calculate percentage of all styles and genres in your collection
+
+    # User Input
     while True:
         cmd = input("Command: ")
         if cmd.lower() == 'k':
             print(f"Style Keys: {' | '.join(style_list)}\n")
             print(f"Genre Keys: {' | '.join(genre_list)}\n")
-        elif cmd.lower() == 's' or cmd.lower() == 'g':
+        elif cmd.lower() in ['s','g','a']:
             display(f_collection, style_list, genre_list, cmd, coll_size)
         elif cmd.lower() == 'q':
             sys.exit()
         elif cmd.lower() == '-h':
-            print("Usage:\nk: print style and genre keys\ns: lookup by style\ng: lookup by genre\nq: quit")
+            print('''Usage:
+            k: Print style and genre sort keys
+            a: Print all records in your collection, sorted by artist name
+            s: Return all records in your collection that match a chosen style, sorted by artist name
+            g: Return all records in your collection that match a chosen genre, sorted by artist name
+            q: Quit''')
         else:
             print("Invalid command.  Enter -h for help")
 
@@ -51,6 +64,7 @@ def main():
 def format_out(coll, g_list, s_list):
     records = []
 
+    # Create a list of Record objects containing relevant data from Discogs API calls
     for i in range(len(coll)):
         for j in range(len(coll[i]['releases'])):
             title = coll[i]['releases'][j]['basic_information']['title']
@@ -75,43 +89,58 @@ def display(coll, s_list, g_list, c, size):
     _opt = ""
     count = 0
 
+    # Program branches here based on command input
     if c == 's':
         sort_type = s_list
         sort_str = "Style"
+    elif c == 'a':
+        sort_type = _opt
+        sort_str = "All"
     else:
         sort_type = g_list
         sort_str = "Genre"
 
-    # TODO: add option to sort by release year
+    # TODO: add option to sort by decade
     while not (_opt in sort_type):
         _opt = input(f"Choose {sort_str}: ")
 
         if _opt.lower() == '-h':
             _opt = input("Usage: Enter a key. For list of keys, Press k. Input a key or press enter to continue. ")
-
-        if _opt.lower() == 'k':
+        elif _opt.lower() == 'k':
             if c == 's':
                 print(f" Styles: {' | '.join(s_list)}")
             else:
                 print(f" Genres: {' | '.join(g_list)}")
+        elif _opt.lower() == 'q':
+            sys.exit()
         else:
             pass
 
+    # Print records to screen in easy to read format
     for record in coll:
-        if c == 's' and _opt in record.styles:
+        if c == 'a':
+            print(f"{count + 1}. {record.artist} - {record.title}\n\tStyles: {' | '.join(record.styles)}\n\tGenres: " +
+                  f"{' | '.join(record.genres)}")
+        elif c == 's' and _opt in record.styles:
             print(f"{count + 1}. {record.artist} - {record.title} --- ({' | '.join(record.styles)})")
-            count += 1
         elif c == 'g' and _opt in record.genres:
             print(f"{count + 1}. {record.artist} - {record.title} --- ({' | '.join(record.genres)})")
-            count += 1
         else:
+            count -= 1
             pass
+        count += 1
 
     print("---------------------------------------------------------------")
-    print("Total: {0}. Percentage of Collection = {1:.2f} %".format(count, (100*count/size)))
+    print(f"Total: {count}", end="")
+
+    if not(c == 'a'):
+        print(". Percentage of Collection = {0:.2f} %".format(100*count/size))
+    else:
+        print("")
 
 
 def error_check(res):
+    # Handling respones other than OK
     if not(res.status_code == 200):
         print("An Error Occurred.  Please Try Again.")
         print(f"Code {res.status_code}: {res.reason}.")
